ea603299bfb599175781931a25e54b29d2aacdcc
Pablo Castellano <pablo@anche.no>
2013-07-05 23:40:40 +0200

    New GuifiNodeMarker class, which associates points and node ids

--- champlainguifinet.py
+++ champlainguifinet.py
@@ -9,9 +9,9 @@ import os
 import sys
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append('lib')
+sys.path.append('lib/libcnml')
 
-from libcnml import Status
-
+from libcnml import CNMLParser, Status
 from utils import APP_NAME, LOCALE_DIR
 
 import gettext
@@ -20,45 +20,36 @@ gettext.textdomain(APP_NAME)
 _ = gettext.gettext
 
 
-# It's compound of two ChamplainMarkerLayer layers
-class GtkGuifinetMap(GtkChamplain.Embed):
-    def __init__(self, parent):
-        GtkChamplain.Embed.__init__(self)
-        self.set_size_request(640, 480)
+class GuifiNodeMarker:
+    def __init__(self, nid, lat, lon, status, size=8):
+        self.double_click_cb = None
+        self.id = nid
+        self.point = Champlain.Point.new()
+        self.point.set_location(lat, lon)
+        self.point.set_size(size)
+        color = self.color_from_status(status)
+        self.point.set_color(color)
+        self.set_double_click_cb(self.point_button_release_cb)  # default callback
 
-        self.parent = parent
-        self.view = self.get_view()
-        self.view.set_reactive(True)
-        self.view.set_kinetic_mode(True)
-        self.view.set_zoom_level(13)
-        self.view.center_on(36.72341, -4.42428)  # TODO: Set to center of default zone
-        self.view.connect('button-release-event', self.mouse_click_cb)
+    @staticmethod
+    def fromCNML(cnmlnode):
+        """
+        Instantiate GuifiNodeMarker from libcnml node structure
+        """
+        nid, lat, lon, status = cnmlnode.id, cnmlnode.latitude, cnmlnode.longitude, cnmlnode.status
+        marker = GuifiNodeMarker(nid, lat, lon, status)
+        return marker
 
-        scale = Champlain.Scale()
-        scale.connect_view(self.view)
-        self.view.bin_layout_add(scale, Clutter.BinAlignment.START, Clutter.BinAlignment.END)
+    def get_champlain_point(self):
+        return self.point
 
-        self.points_layer = Champlain.MarkerLayer()
-        self.points_layer.set_selection_mode(Champlain.SelectionMode.SINGLE)
-        self.labels_layer = Champlain.MarkerLayer()
+    def set_double_click_cb(self, func):
+        if self.double_click_cb:
+            self.point.disconnect_by_func(self.double_click_cb)
+        self.point.connect('button-release-event', func)
+        self.double_click_cb = func
 
-        # It's important to add points the last. Points are selectable while labels are not
-        # If labels is added later, then you click on some point and it doesn't get selected
-        # because you are really clicking on the label. Looks like an usability bug?
-        self.view.add_layer(self.labels_layer)
-        self.view.add_layer(self.points_layer)
-
-        self.menu = Gtk.Menu()
-        menuitem1 = Gtk.MenuItem('Create new Guifi.net node here')
-        menuitem1.connect('activate', self.create_new_node)
-        self.menu.append(menuitem1)
-        self.menu.show_all()
-
-    def create_new_node(self, action, data=None):
-        self.parent.create_new_node((self.lat, self.lon))
-        del self.lat, self.lon
-
-    def colorFromStatus(self, status):
+    def color_from_status(self, status):
         # Planned: blue
         # Working: green
         # Testing: orange
@@ -82,18 +73,73 @@ class GtkGuifinetMap(GtkChamplain.Embed):
 
         return color
 
-    def add_node_point(self, lat, lon, status, size=12):
-        p = Champlain.Point.new()
-        p.set_location(lat, lon)
-        p.set_size(size)
-        color = self.colorFromStatus(status)
-        p.set_color(color)
-        self.points_layer.add_marker(p)
+    # TODO: This shouldn't be in the view but in the controller
+    def point_button_release_cb(self, widget, event):
+        """
+        Default double-click handler
+        """
+        if event.button == 1 and event.click_count > 1:
+            print 'Marcador pulsado'
+            from ui import NodeDialog
+            NodeDialog()
+        return True
 
-    def add_node_label(self, lat, lon, text, status):
+
+# It's compound of two ChamplainMarkerLayer layers
+class GtkGuifinetMap(GtkChamplain.Embed):
+    def __init__(self, parent):
+        GtkChamplain.Embed.__init__(self)
+        self.set_size_request(640, 480)
+
+        self.parent = parent
+        self.view = self.get_view()
+        self.view.set_reactive(True)
+        self.view.set_kinetic_mode(True)
+        self.view.set_zoom_level(13)
+        self.view.center_on(36.72341, -4.42428)  # TODO: Set to center of default zone
+        self.view.connect('button-release-event', self.mouse_click_cb)
+
+        scale = Champlain.Scale()
+        scale.connect_view(self.view)
+        self.view.bin_layout_add(scale, Clutter.BinAlignment.START, Clutter.BinAlignment.END)
+
+        self.points = {}
+        self.points_layer = Champlain.MarkerLayer()
+        self.points_layer.set_selection_mode(Champlain.SelectionMode.SINGLE)
+        self.labels_layer = Champlain.MarkerLayer()
+
+        # It's important to add points the last. Points are selectable while labels are not
+        # If labels is added later, then you click on some point and it doesn't get selected
+        # because you are really clicking on the label. Looks like an usability bug?
+        self.view.add_layer(self.labels_layer)
+        self.view.add_layer(self.points_layer)
+
+        self.menu = Gtk.Menu()
+        menuitem1 = Gtk.MenuItem(_('Create new Guifi.net node here'))
+        menuitem1.connect('activate', self.create_new_node_cb)
+        self.menu.append(menuitem1)
+        self.menu.show_all()
+
+    def get_point_from_nid(self, nid):
+        """
+        Return Champlain.Point instance of node with id "nid"
+        """
+        return self.points[nid]
+
+    def create_new_node_cb(self, action, data=None):
+        self.parent.create_new_node((self.lat, self.lon))
+        del self.lat, self.lon
+
+    def add_node_point(self, cnmlnode, size=8):
+        p = GuifiNodeMarker.fromCNML(cnmlnode)
+        self.points_layer.add_marker(p.get_champlain_point())
+        self.points[cnmlnode.id] = p
+
+    def add_node_label(self, cnmlnode):
+        nid, lat, lon, text, status = cnmlnode.id, cnmlnode.latitude, cnmlnode.longitude, cnmlnode.title, cnmlnode.status
+
         p = Champlain.Label.new()
         p.set_text(text)
-        #color = self.colorFromStatus(status)
         color = Clutter.Color.new(0, 0, 0, 255)
         p.set_text_color(color)
         p.set_location(lat, lon)
@@ -151,6 +197,7 @@ class GtkGuifinetMap(GtkChamplain.Embed):
         else:
             self.labels_layer.hide_all_markers()
 
+    # TODO: This shouldn't be in the view but in the controller
     def mouse_click_cb(self, widget, event):
         # event == void (GdkEventButton?)
         if event.button == 3:  # Right button
